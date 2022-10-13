4597dd281e0561ccb906223dd73ffaf9b1c5b034
LssLn <lssln97@gmail.com>
2022-10-13 22:47:37 +0200

    login - user test

---
+++ src/test/java/com/example/loginApp/UserRepositoryTests.java
@@ -0,0 +1,39 @@
+package com.example.loginApp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.example.loginApp.entity.User;
+import com.example.loginApp.repository.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		user.setEmail("lssln97@gmail.com");
+		user.setPassword("password12");
+		user.setFirstName("LSS");
+		user.setLastName("LN");
+		
+		User userSaved = userRepository.save(user);
+		User userFound = entityManager.find(User.class, userSaved.getId());
+		
+		assertThat(userFound.getEmail()).isEqualTo(user.getEmail());
+	}
+}
